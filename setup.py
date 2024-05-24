e3c609f046ebfe86f5340d32144ac5b441ffa87a
Mohamad Nematizadeh <mohammad.nematizzadeh@gmail.com>
2024-05-24 17:15:08 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from pathlib import Path
 from setuptools import setup
 
 
-def post_install():
-    text = "#detect_dice is a simple and fast dice detection library made in Python based on cv2(opencv. This library allows you to perform dice detection operations on images for you"
 
+def pre_install():
+    text = "#detect_dice is a simple and fast dice detection library made in Python based on cv2(opencv. This library allows you to perform dice detection operations on images for you"
     return text
 
 pre_install()
@@ -13,8 +13,7 @@ setup(name="detect_dice" ,
     version="1.0.4", 
     author="MohammadNematizadeh" , 
     description="This package is in the field of image processing and can perform dice detection for you",
-    long_description==post_install(),
+    long_description=pre_install(),
     requires= [] ,
     author_email="mohammad.nematizzadeh@gmail.com",
-    packages=["detect_dice"],
-    )
+    packages=["detect_dice"])
