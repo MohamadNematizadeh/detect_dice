f195f5f3abae891212b667b097ac2b64af21350e
Mohamad Nematizadeh <mohammad.nematizzadeh@gmail.com>
2024-05-24 22:59:57 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from pathlib import Path
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-
-def pre_install():
-    text = "#detect_dice is a simple and fast dice detection library made in Python based on cv2(opencv. This library allows you to perform dice detection operations on images for you"
-    return text
-
-pre_install()
 
 setup(name="detect_dice" ,
-    version="1.0.4", 
-    author="MohammadNematizadeh" , 
-    description="This package is in the field of image processing and can perform dice detection for you",
-    long_description=pre_install(),
-    requires= [] ,
+    version="1.0.10", 
+    author="MohammadNematizadeh" ,
     author_email="mohammad.nematizzadeh@gmail.com",
-    packages=["detect_dice"])
+    description="This package is in the field of image processing and can perform dice detection for you",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MohamadNematizadeh/detect_dice",
+    requires= [] ,
+    packages=["detect_dice"],
+    )
+
