03f265535024a2f2d023e9efb8af9e8fc542cba5
Mohamad Nematizadeh <mohammad.nematizzadeh@gmail.com>
2024-05-24 16:43:12 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,31 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from pathlib import Path
 
+
+
+
+def post_install():
+    """ Implement post installation routine """
+    with open('./requirements.txt') as f:
+        install_requires = f.read().splitlines()
+
+    return install_requires
+
+
+def pre_install():
+    """ Implement pre installation routine """
+    # read the contents of your README file
+    global long_description
+    this_directory = Path(__file__).parent
+    long_description = (this_directory / "README.md").read_text()
+
+
+pre_install()
 
 setup(name="detect_dice" ,
     version="1.0.4", 
     author="MohammadNematizadeh" , 
     description="This package is in the field of image processing and can perform dice detection for you",
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
     long_description=pre_install() ,
     requires= [] ,
     author_email="mohammad.nematizzadeh@gmail.com",
